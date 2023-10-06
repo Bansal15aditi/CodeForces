26f0196eb332e17c5bbe148aa0de46b5cddd105b
Aditi Bansal <100828059+Bansal15aditi@users.noreply.github.com>
2023-10-07 00:44:03 +0530

    Add files via upload

---
+++ calculating_function.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class calculating_function {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        if(n%2==0){
+            System.out.println(n/2);
+        }
+        else{
+            System.out.println((n/2)-n);
+        }
+    }
+}
